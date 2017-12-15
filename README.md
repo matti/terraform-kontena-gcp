36214b146eba16820b67e7ab4e53392c3ae583a5
Matti Paksula <matti.paksula@iki.fi>
2017-12-15 11:59:16 +0200

    :sunrise:

---
+++ README.md
@@ -0,0 +1,9 @@
+# terraform-kontena-gcp
+
+Kontena on Google Cloud example, see main.tf
+
+## Usage:
+
+```
+$ GOOGLE_PROJECT=todo GOOGLE_REGION=us-east1 GOOGLE_APPLICATION_CREDENTIALS=~/path/to/service_account.json terraform plan
+```
---
+++ main.tf
@@ -0,0 +1,39 @@
+module "kontena_node_ignition" {
+  source = "github.com/kontena/kontena-terraform-modules/modules/node-ignition"
+
+  master_uri  = "wss://TODO-TODO-123.platforms.us-east-1.kontena.cloud"
+  grid_token  = "TODOTODOTODOTODO=="
+  docker_opts = "--label provider=gcp --label region=us-east1"
+
+  dns_server            = "169.254.169.254" # Google's DNS
+  overlay_version       = "overlay"         # or overlay2
+  peer_interface        = "ens4v1"
+  main_interface_prefix = "ens4v"
+
+  authorized_keys_core = [
+    "ssh-rsa TODOTODO....",
+  ]
+}
+
+data "google_compute_image" "coreos_stable" {
+  family  = "coreos-stable"
+  project = "coreos-cloud"
+}
+
+module "gci_nodes" {
+  source = "github.com/matti/terraform-google-compute-instance"
+
+  amount = 3
+  region = "us-east1"
+  name   = "test"
+
+  # Can be changed without destroying the disk
+  machine_type = "custom-2-2048"
+  disk_size    = 32
+  disk_image   = "${data.google_compute_image.coreos_stable.self_link}"
+  user_data    = "${module.kontena_node_ignition.rendered}"
+}
+
+output "ips" {
+  value = "${module.gci_nodes.instance_ips}"
+}
